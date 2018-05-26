58d284823d487ab60a0c431afcf0e1ed6cfa4a2f
drmaluta <admin@bardsong.com>
2018-05-26 09:27:46 -0700

    Completed all TODO tasks

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,31 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by admin on 5/26/2018.
+ */
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static String LOG_TAG = SquawkFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // This method is blank, but if you were to build a server that stores users token
+        // information, this is where you'd send the token to the server.
+    }
+}
